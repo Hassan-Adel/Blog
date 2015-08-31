acb19e66883210c6b9e19fa6ea02b678d007c3b9
Hassan-Adel <hasanadel555@hotmail.com>
2015-08-31 11:05:05 +0200

    Final commit

--- app/src/main/java/com/example/pc/blog/Splash.java
+++ app/src/main/java/com/example/pc/blog/Splash.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
 
 public class Splash extends Activity {
 
@@ -14,7 +17,10 @@ public class Splash extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash);
+
+
 
         ActionBar Act=getActionBar();
         Act.hide();
@@ -28,6 +34,8 @@ public class Splash extends Activity {
                 }finally{
                     Intent i = new Intent(Splash.this,privatepublic.class);
                     startActivity(i);
+                    overridePendingTransition(0, 0);
+                    finish();
                 }
             }
         };
